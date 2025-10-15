e3d66f85a26f4c9900d8d282c1f69e6da484bf88
adjustment-commits <adjustment.h.nishimoto@gmail.com>
2025-10-15 20:57:23 +0900

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,44 @@
+const CACHE_NAME = "vo2max-white-v1";
+const urlsToCache = [
+  "./index.html",
+  "./manifest.json",
+  "./service-worker.js",
+  "./icons/icon-192.png",
+  "./icons/icon-512.png"
+];
 
+// インストール時にキャッシュ
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+  self.skipWaiting();
+});
+
+// リクエスト取得時のキャッシュ応答
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return (
+        response ||
+        fetch(event.request).then(res => {
+          return caches.open(CACHE_NAME).then(cache => {
+            cache.put(event.request, res.clone());
+            return res;
+          });
+        })
+      );
+    })
+  );
+});
+
+// 古いキャッシュ削除
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+      )
+    )
+  );
+});
